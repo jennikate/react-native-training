5482c2033b39de72bca51406e8ff6e03bec50585
JenniKate Wallace <jennikate@gmail.com>
2023-04-11 09:46:00 +0100

    Show both types of styling options

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+/* 
+ * Style props are supported on View and Text : they can be inline or stylesheet objects (sso's are the better option for same reason as we do with CSS stylesheet)
+ * margin takes a number that's converted to the device pixel sizing
+ * border does not have a shorthand like in css, you need to specify each item
+ * borderColour takes a string, which can be a hex value or a word like 'red'
+ */
+
 export default function App() {
   console.log('logs');
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another text</Text>
+        <Text style={styles.dummyText}>Another text</Text>
       </View>
-      <Text>Hello world</Text>
+      <Text style={{margin: 16, borderWidth: 2, borderColor: 'red', padding: 16}}>Hello world</Text>
       <Button title="Tap me" />
     </View>
   );
@@ -20,4 +27,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding: 16
+  }
 });
